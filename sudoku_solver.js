df67dc966a072a4cc4458a2a7a3bd850e82d5806
Scotty Vernon <scott.vernon@gmail.com>
2014-06-25 19:41:29 +0100

    Write saveEmptyPositions() function.

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -10,3 +10,21 @@ module.exports.parseBoard = function(board) {
     })
   });
 }
+
+module.exports.saveEmptyPositions = function(board) {
+  // create an array to save the positions
+  var emptyPositions = [];
+
+  // check every square in the puzzle for a zero
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      // if a zero is found, save that position
+      if (board[i][j] === 0) {
+        emptyPositions.push([i, j]);
+      }
+    }
+  }
+
+  // return the positions
+  return emptyPositions;
+};

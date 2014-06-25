d258af33059a10b4cfe1912e788c01bff83eb4f8
Scotty Vernon <scott.vernon@gmail.com>
2014-06-25 19:03:13 +0100

    Define sudoku solver spec test.

---
+++ test/sudoku_solver_spec.js
@@ -0,0 +1,11 @@
+// sudoku_solver_spec.js
+
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+var solver = require('../solver');
+
+describe('Sudoku Solver', function() {
+
+});

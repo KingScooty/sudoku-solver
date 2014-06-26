783c7b4501ace59049d0538c841e6e1f3fe5a8ed
Scotty Vernon <scott.vernon@gmail.com>
2014-06-26 23:07:26 +0100

    Write checkValue() function.

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -85,3 +85,13 @@ module.exports.check3x3Square = function(board, column, row, value) {
   // If no match was found, return true
   return true;
 };
+
+module.exports.checkValue = function(board, column, row, value) {
+  if (this.checkRow(board, row, value) &&
+      this.checkColumn(board, row, value) &&
+      this.check3x3Square(board, column, row, value)) {
+    return true;
+  } else {
+    return false;
+  }
+};

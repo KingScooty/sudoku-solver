bf3215f5d5ac06aa19adf76b95a716bca5bc6b7c
Scotty Vernon <scott.vernon@gmail.com>
2014-06-27 18:00:22 +0100

    Fix typo and tidy up.

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -1,5 +1,3 @@
-// sudoku_solver.js
-
 module.exports.parseBoard = function(board) {
   // split the board at each new line, and use map
   // to split each row into an array of characters
@@ -7,53 +5,51 @@ module.exports.parseBoard = function(board) {
     // use map to convert the characters into integers
     return row.split('').map(function(num) {
       return +num;
-    })
+    });
   });
-}
+};
 
 module.exports.saveEmptyPositions = function(board) {
-  // create an array to save the positions
+  // Create an array to save the positions
   var emptyPositions = [];
 
-  // check every square in the puzzle for a zero
-  for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board[i].length; j++) {
-      // if a zero is found, save that position
-      if (board[i][j] === 0) {
+  // Check every square in the puzzle for a zero
+  for(var i = 0; i < board.length; i++) {
+    for(var j = 0; j < board[i].length; j++) {
+      // If a zero is found, so that position
+      if(board[i][j] === 0) {
         emptyPositions.push([i, j]);
       }
     }
   }
 
-  // return the positions
+  // Return the positions
   return emptyPositions;
 };
 
 module.exports.checkRow = function(board, row, value) {
   // Iterate through every value in the row
-  for (var i = 0; i < board[row].length; i++) {
+  for(var i = 0; i < board[row].length; i++) {
     // If a match is found, return false
-    if (board[row][i] === value) {
+    if(board[row][i] === value) {
       return false;
     }
   }
-
   // If no match was found, return true
   return true;
 };
 
 module.exports.checkColumn = function(board, column, value) {
-  // Iterate through every value in the column
-  for (var i = 0; i < board.length; i++) {
+  // Iterate through each value in the column
+  for(var i = 0; i < board.length; i++) {
     // If a match is found, return false
-    if (board[i][column] === value) {
+    if(board[i][column] === value) {
       return false;
     }
   }
-
   // If no match was found, return true
   return true;
-}
+};
 
 module.exports.check3x3Square = function(board, column, row, value) {
   // Save the upper left corner
@@ -62,34 +58,33 @@ module.exports.check3x3Square = function(board, column, row, value) {
       squareSize = 3;
 
   // Find the left-most column
-  while (column >= columnCorner + squareSize) {
+  while(column >= columnCorner + squareSize) {
     columnCorner += squareSize;
   }
 
   // Find the upper-most row
-  while (row >= rowCorner + squareSize) {
+  while(row >= rowCorner + squareSize) {
     rowCorner += squareSize;
   }
 
-  // Iterate through each row 
-  for (var i = rowCorner; i < rowCorner + squareSize; i++) {
+  // Iterate through each row
+  for(var i = rowCorner; i < rowCorner + squareSize; i++) {
     // Iterate through each column
-    for (var j = columnCorner; j < columnCorner + squareSize; j++) {
-      // Return false if a match is found
-      if (board[i][j] === value) {
+    for(var j = columnCorner; j < columnCorner + squareSize; j++) {
+      // Return false is a match is found
+      if(board[i][j] === value) {        
         return false;
       }
     }
   }
-
   // If no match was found, return true
   return true;
 };
 
 module.exports.checkValue = function(board, column, row, value) {
-  if (this.checkRow(board, row, value) &&
-      this.checkColumn(board, row, value) &&
-      this.check3x3Square(board, column, row, value)) {
+  if(this.checkRow(board, row, value) &&
+    this.checkColumn(board, column, value) &&
+    this.check3x3Square(board, column, row, value)) {
     return true;
   } else {
     return false;
